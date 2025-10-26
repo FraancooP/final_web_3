375a03c1055c1bd6a7c0c16e950d9eb5028464c2
franco <franco>
2025-10-26 17:33:17 -0300

    Se tabajo con Orden implementacion mas interfaz

--- MavenIW3/iw3/src/main/java/com/tpfinal/iw3/model/business/interfaces/IOrdenBusiness.java
+++ MavenIW3/iw3/src/main/java/com/tpfinal/iw3/model/business/interfaces/IOrdenBusiness.java
@@ -1,5 +1,25 @@
 package com.tpfinal.iw3.model.business.interfaces;
 
-public interface IOrdenBusiness {
+public List<Orden> list() throws BusinessException;
+
+    public Orden load(long id) throws NotFoundException, BusinessException;
+
+    public Orden add(Orden orden) throws FoundException, BusinessException;
+
+    public Orden update(Orden orden) throws NotFoundException, BusinessException, FoundException;
+
+    public void delete(Orden orden) throws NotFoundException, BusinessException;
+
+    public void delete(long id) throws NotFoundException, BusinessException;
+
+    public Orden acknowledgeAlarm(Long idAlarm, User user) throws BusinessException, NotFoundException;
+
+    public Orden confirmIssueAlarm(Long idAlarm, User user) throws BusinessException, NotFoundException;
+
+    public byte[] generateConciliationPdf(Long orderNumber) throws BusinessException, NotFoundException;
+
+    public Map<String, Object> getConciliationJson(Long idOrder) throws BusinessException, NotFoundException;
+
+
 
 }
