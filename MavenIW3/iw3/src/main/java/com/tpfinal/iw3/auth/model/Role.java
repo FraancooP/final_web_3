30b70bb9614e209ff8fb0c690b2b652e8d3393f9
joa <joa>
2025-10-26 17:34:13 -0300

    Se trabajo en auth

--- MavenIW3/iw3/src/main/java/com/tpfinal/iw3/auth/model/Role.java
+++ MavenIW3/iw3/src/main/java/com/tpfinal/iw3/auth/model/Role.java
@@ -1,5 +1,35 @@
 package com.tpfinal.iw3.auth.model;
 
-public class Role {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name =  "roles")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = -845420067971973620L;
+
+    @Column(nullable = false, length = 100)
+    private String descripcion;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique=true, nullable = false)
+    private String nombre;
 
 }
--- MavenIW3/iw3/src/main/java/com/tpfinal/iw3/auth/model/persistence/UserRepository.java
+++ MavenIW3/iw3/src/main/java/com/tpfinal/iw3/auth/model/persistence/UserRepository.java
@@ -1,5 +1,11 @@
 package com.tpfinal.iw3.auth.model.persistence;
 
-public class UserRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{ 
+	public Optional<User> findOneByUsernameOrEmail(String username, String email);
 }
