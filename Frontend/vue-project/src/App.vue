bb2752bce41f09d978b0a2ad1322ddf570bba95e
you@example.com <you@example.com>
2026-01-27 19:10:02 -0300

    Vue iniciado

---
+++ Frontend/vue-project/src/App.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <h1>FUNCIONA CAPO</h1>
+  <p>
+    Visit <a href="https://videosdegatitos.org/" target="_blank" rel="noopener">videosdegatitos.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
